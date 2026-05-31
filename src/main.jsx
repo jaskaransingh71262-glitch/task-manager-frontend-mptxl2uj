dc29560d77ab77de3782243d949ea05c0c5f5e13
jaskaransingh71262-glitch <jaskaransingh71262@gmail.com>
2026-05-31 20:56:15 +0530

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react';import ReactDOM from 'react-dom';import { BrowserRouter } from 'react-router-dom';import App from './App.jsx';import 'bootstrap/dist/css/bootstrap.min.css';ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>);
